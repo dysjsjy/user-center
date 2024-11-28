f9a0aa76722073e187ac161a4c63b01063d829ef
dysjsjy <dysjsjy@163.com>
2024-11-28 21:31:22 +0800

    暂时保存

---
+++ user-center-frontend-react/src/store/modules/user.ts
@@ -0,0 +1,13 @@
+//用户相关的状态管理
+
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const userStore = createSlice({
+    name: "user",
+    initialState: undefined,
+    reducers: {}
+})
+
+export default userStore.reducer
